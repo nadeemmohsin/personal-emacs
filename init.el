9518b70d2122dfaee69bf3b8f2482c4dc481f205
Nadeem Mohsin <nadeemm@verily.com>
2023-01-29 17:15:38 -0800

    Separate custom vars file.

--- init.el
+++ init.el
@@ -14,6 +14,9 @@
 ;; Put backup files in a separate directory.
 (setq backup-directory-alist `(("." . "~/.emacs.bak")))
 
+;; Keep customized variables in a separate file.
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
 
 ;; Initialize package archives.
 (require 'package)
@@ -188,19 +191,3 @@
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
-
-;; -------------------------------------------------------------------------------- ;; 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-minibuffer-history-key "M-p")
- '(package-selected-packages
-   '(consult orderless marginalia vertico rainbow-delimiters magit counsel-projectile projectile company-box vterm ace-window dired-hide-dotfiles dired-open all-the-icons-dired dired-single evil-collection evil doom-modeline all-the-icons doom-themes helpful ivy-prescient counsel ivy-rich ivy use-package zenburn-theme yasnippet-snippets yaml-mode which-key undo-tree tabbar session rust-mode puppet-mode pod-mode muttrc-mode mutt-alias lsp-mode initsplit ido-completing-read+ htmlize graphviz-dot-mode goto-chg gitignore-mode gitconfig-mode gitattributes-mode git-modes folding flycheck ess eproject editorconfig diminish csv-mode company color-theme-modern browse-kill-ring boxquote bm bar-cursor apache-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
